f2b2c2c1fa1768bf7a960399e5dcd0094d22e27c
Thomas Ochman <thomas@dragster.se>
2022-03-03 11:41:09 +0100

    WIP attepts to style the menu display

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,24 +6,28 @@ const Menu = () => {
 
   const fetchProducts = async () => {
     const result = await axios.get("http://localhost:3000/api/products");
-    setProducts(result.data.products)
+    setProducts(result.data.products);
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-
-  return <div data-cy="product-list">
-
-    {products.map(product => (  
-      <div key={product.id}>
-        <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        <p>{product.price}</p>
-      </div>
-    ))}
-  </div>;
+  return (
+    <div data-cy="product-list" style={{ display: "grid", gridTemplateColumns: 'auto auto auto' }}>
+      {products.map((product) => (
+        <div>
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+          </div>
+          <div>
+            <p>{product.price} kr</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
